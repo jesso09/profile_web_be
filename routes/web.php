7dc2fae9965162b489e40b22751b49a9df3e640c
Gede Bagus Jyestha Permana <106970238+jesso09@users.noreply.github.com>
2024-11-01 22:17:29 +0800

    disable symlink

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/symlink', function () {
-    $target =$_SERVER['DOCUMENT_ROOT'].'/storage/app/public';
-    $link = $_SERVER['DOCUMENT_ROOT'].'/public/storage';
-    symlink($target, $link);
-    echo "Done";
- });
+// Route::get('/symlink', function () {
+//     $target =$_SERVER['DOCUMENT_ROOT'].'/storage/app/public';
+//     $link = $_SERVER['DOCUMENT_ROOT'].'/public/storage';
+//     symlink($target, $link);
+//     echo "Done";
+//  });
